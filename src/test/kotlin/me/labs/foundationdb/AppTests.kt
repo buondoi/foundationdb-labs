256f9c7dc1ea0cb18ff5904cfd11b56c86db8cba
tamnguyen <tamnguyen1560@gmail.com>
2018-05-10 23:29:27 +0700

    Initial commit

---
+++ src/test/kotlin/me/labs/foundationdb/AppTests.kt
@@ -0,0 +1,15 @@
+package me.labs.foundationdb
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class AppTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
